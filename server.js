0b787d098e020a62c279ebbd226d93a14a8c3424
DavidNatanael <davidstinghen@gmail.com>
2019-05-20 16:17:34 -0300

    liberando acesso a api por outros dominios

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 // iniciando o app
 const app = express();
 
 // dando permissão para eviar dados no formato json
 app.use(express.json());
+
+//liberando acesso a api por outros dominios
+app.use(cors());
 
 // conectando o banco de dados
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
